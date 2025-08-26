fb6ced2c9a569df7726b7887fa22cacb86a9e774
imamanjangra <imamanjangra@gmail.com>
2025-08-26 23:38:52 +0530

    60% complete

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,12 @@
 let menu_btn = document.querySelector(".menu");
 let menu_data = document.querySelector(".menu_data");
 let same_a = document.querySelectorAll(".same_a");
+let dark_mode = document.querySelector(".dark_mode");
+let body = document.querySelector("body");
+let t_w = document.querySelectorAll(".t_w");
+let nav = document.querySelector(".nav");
+let moon = document.querySelector(".moon");
+
 
 let menu_v = true;
 menu_btn.addEventListener("click" , ()=>{
@@ -13,3 +19,50 @@ same_a.forEach(btn => {
         menu_data.classList.add("hidden");
     })
 });
+
+// dark mode 
+let dark_value = true;
+function darkmode(){
+    if(dark_value){
+    console.log("dark btn was clicked")
+    body.style.backgroundColor = "#0B1020";
+
+    t_w.forEach(i => {
+        i.style.color = "white";
+    });
+
+    nav.classList.remove("bg-white/50"); // remove white
+    nav.classList.add("bg-[#0B1020]/50"); // add dark
+    
+    dark_mode.classList.add("hidden")
+    moon.classList.remove("hidden")
+    
+    dark_value = false;
+    }
+    else{
+        body.style.backgroundColor = "white"
+        t_w.forEach(i => {
+        i.style.color = "black";
+        
+    });
+
+    nav.classList.remove("bg-[#0B1020]/50"); // remove dar
+    nav.classList.add("bg-white/50"); // add white
+   
+
+    dark_mode.classList.remove("hidden")
+    moon.classList.add("hidden");
+    
+
+        dark_value = true;
+
+    }
+}
+
+dark_mode.addEventListener("click" , ()=>{
+    darkmode();
+})
+
+moon.addEventListener('click'  , ()=>{
+    darkmode();
+})
